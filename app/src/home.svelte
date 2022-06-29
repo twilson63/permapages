67d805488591bd6c5d8cc524949b88f28d809461
Tom Wilson <tom@hyper.io>
2022-06-29 02:30:30 +0000

    updated home page

--- app/src/home.svelte
+++ app/src/home.svelte
@@ -3,33 +3,55 @@
   let version = __APP_VERSION__.split(".")[2];
 </script>
 
-<div class="hero min-h-screen bg-base-200">
+<div class="hero min-h-screen bg-base-200 items-start">
   <div class="hero-content text-center">
-    <div class="max-w-md">
+    <div class="w-full">
       <figure class="flex items-center justify-center">
-        <img class="w-64 rounded-lg" src="permapages_logo.svg" alt="logo" />
+        <img
+          class="h-[200px] rounded-lg"
+          src="permapages_logo.svg"
+          alt="logo"
+        />
       </figure>
 
-      <h1 class="mt-8 text-5xl">
-        <span class="uppercase text-primary">🐘 PermaPages 🐘</span>
+      <h1 class="mt-8 text-5xl text-left">
+        <span class="uppercase text-primary">Welcome to Permapages!</span>
       </h1>
-      <div class="badge badge-secondary">BETA {version}</div>
-      <div class="flex mt-8 space-x-2">
-        <h2 class="text-3xl text-secondary">✨ BUILD</h2>
-        <h2 class="text-3xl text-primary">🚀 PUBLISH</h2>
-        <h2 class="text-3xl text-accent">⚡️ CONNECT</h2>
-      </div>
-      <div class="mt-16 space-x-4">
-        {#if $address}
-          <a href="/pages" class="btn btn-primary">Pages</a>
-        {:else}
-          <a href="/connect" class="btn btn-primary">Connect</a>
-        {/if}
-        <a
-          target="_blank"
-          href="https://permanotes.app/#/notes/WYfC1LPyHJlHrTaN11QS_9-rDMXW0EREqp3FlYKzIWE"
-          class="btn btn-outline btn-secondary">Learn More</a
-        >
+      <div class="flex space-x-16">
+        <div class="flex mt-8 space-x-2 flex-col justify-start text-left w-1/2">
+          <p class="text-xl">
+            Permapages is a Web3 app that allows users to create and manage
+            their own permanent web3 profile and other permaweb pages. When
+            connecting to the application with your wallet, you can create your
+            Web3 profile by uploading an avatar and entering some social link
+            information. Then you can use the editor to create content and add
+            permapage widgets to make your profile fun and unique.
+          </p>
+        </div>
+        <div class="mt-16 space-x-4">
+          <div class="mb-16 mx-8">
+            <div class="text-2xl text-primary">
+              Connect your wallet to claim your page on the permaweb
+            </div>
+          </div>
+          <div class="flex justify-center space-x-4">
+            {#if $address}
+              <a href="/pages" class="btn btn-primary rounded w-[200px]"
+                >Pages</a
+              >
+            {:else}
+              <a href="/connect" class="btn btn-primary rounded w-[200px]"
+                >Connect Wallet</a
+              >
+            {/if}
+            <a
+              target="_blank"
+              href="https://permanotes.app/#/notes/WYfC1LPyHJlHrTaN11QS_9-rDMXW0EREqp3FlYKzIWE"
+              class="btn btn-outline rounded w-[200px] btn-primary"
+              >Learn More</a
+            >
+          </div>
+        </div>
       </div>
     </div>
   </div>
