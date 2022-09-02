9cb4cc0e16eca064a14dab9ef6d82ca0503a5075
Tom Wilson <tom@hyper.io>
2022-09-02 19:56:02 +0000

    added dark overrides

--- app/src/pages/form.svelte
+++ app/src/pages/form.svelte
@@ -192,7 +192,7 @@
 
       page.html = `<div class="flex flex-col md:flex-row space-x-4">  
         <div class="flex-1 md:min-h-screen">
-          <div class="prose md:prose-lg lg:prose-xl m-8 md:mx-24">
+          <div class="prose md:prose-lg lg:prose-xl m-8 md:mx-24 dark:text-[#D3D7E0] dark:prose-headings:text-[#D3D7E0] dark:prose-headings:text-[#D3D7E0]">
           ${marked.parse(page.content)}</div></div>
         <div class="flex-none">
           <div class="flex flex-col max-w-[300px] justify-end space-y-8">
