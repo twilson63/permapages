fc5ca85150faab85c927af8c6f8682c5858f8400
Tom Wilson <tom@hyper.io>
2022-09-27 14:22:19 +0000

    update fix to arConnect

--- app/src/main.js
+++ app/src/main.js
@@ -7,6 +7,7 @@ window.addEventListener("arweaveWalletLoaded", async () => {
   if (localStorage.getItem("address") !== "") {
     if (arweaveWallet) {
       try {
+        await arweaveWallet.disconnect()
         await arweaveWallet.connect(
           ["ACCESS_ADDRESS", "SIGN_TRANSACTION", "DISPATCH"],
           {
