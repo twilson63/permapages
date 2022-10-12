a012a3adc35b7c48d1bdb35430e87e39ad819d21
mrsaifullah52 <mrsaifullah52@gmail.com>
2022-10-12 23:19:06 +0500

    componse landing page added

---
+++ app/src/home4.svelte
@@ -0,0 +1,27 @@
+<div class="min-h-screen bg-[#364045] ">
+  <div class="flex justify-end p-4">
+    <a href="/#/#" class="inline-block text-right text-white">Explore Pages</a>
+  </div>
+
+  <div class="w-full flex flex-col items-center justify-center">
+    <img src="magic-hand.png" alt="magic-hand" width="130px" />
+    <h1
+      class="text-8xl text-transparent font-bold bg-clip-text bg-gradient-to-r from-[#FF00E5] to-[#7B55EC]"
+    >
+      compose
+    </h1>
+    <p class="text-[#F7F7F7] font-semibold mt-4 tracking-widest">
+      your creative portal to the permaweb
+    </p>
+
+    <button
+      class="text-[#F7F7F7] border-4 rounded-full p-2 px-10 font-semibold mt-12 "
+      >connect</button
+    >
+    <a href="/#/#" class="text-[#F7F7F7] inline-block mt-5">learn more</a>
+  </div>
+
+  <div class="">
+    <img src="wave img.png" alt="wave" />
+  </div>
+</div>
