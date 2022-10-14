9c3e0ebbd3ccf3a1c11dc904654b4b7fa67891bb
mrsaifullah52 <mrsaifullah52@gmail.com>
2022-10-15 03:09:35 +0500

    dashboard balances separated

--- app/src/pages/index2.svelte
+++ app/src/pages/index2.svelte
@@ -1,4 +1,5 @@
 <script>
+  import DashboardBalances from "../components/dashboard-balances.svelte";
   import NavBar from "../components/navbar.svelte";
   import PagesCards from "../components/pages-cards.svelte";
 
@@ -28,7 +29,6 @@
 </script>
 
 <NavBar />
-
 <main class="container mx-auto min-h-screen">
   <div class="w-full">
     <div class="flex flex-row mt-10">
@@ -79,93 +79,7 @@
         </div>
       </div>
 
-      <div
-        class="w-[52%] border-4 rounded-3xl border-dashed border-gray-200 p-1 flex flex-row items-start"
-      >
-        <div class="h-full bg-gray-50 rounded-3xl p-4 px-5">
-          <h4 class="text-gray-400 text-lg font-semibold mt-4">
-            Wallet balance
-          </h4>
-          <p class="text-black text-xl mt-2 font-semibold tracking-wide">
-            $AR 0.<span class="text-[#6F9986]">0017</span>
-          </p>
-
-          <div class="w-full flex flex-row gap-2 mt-2">
-            <img src="shield-badge.png" alt="shield-badge" width="15px" />
-            <span class="text-xs text-gray-400">connected</span>
-          </div>
-
-          <button
-            class="px-6 py-2 bg-gray-200 text-black rounded-full mt-4 font-semibold drop-shadow-sm hover:bg-gray-300"
-            >Buy $AR</button
-          >
-        </div>
-
-        <div class="h-full p-4 px-6">
-          <div class="w-full">
-            <h4
-              class="text-gray-400 text-lg font-semibold mt-4 flex flex-row items-center gap-1"
-            >
-              <span>Burned AR</span>
-              <button
-                ><img
-                  src="info-icon.png"
-                  alt="info-icon"
-                  width="18px"
-                /></button
-              >
-            </h4>
-            <p class="text-black text-xl mt-2 font-semibold tracking-wide">
-              $bAR 67.<span class="text-[#6F9986]">0017</span>
-            </p>
-          </div>
-
-          <div class="w-full mt-auto">
-            <h4
-              class="text-gray-400 text-lg font-semibold mt-5 flex flex-row items-center gap-1"
-            >
-              <span>Stamp Coin</span>
-              <button
-                ><img
-                  src="info-icon.png"
-                  alt="info-icon"
-                  width="18px"
-                /></button
-              >
-            </h4>
-            <p class="text-black text-xl mt-2 font-semibold tracking-wide">
-              $sCoin 7.<span class="text-[#6F9986]">0017</span>
-            </p>
-          </div>
-        </div>
-
-        <div class="h-full p-4 px-6">
-          <div class="w-full">
-            <h4 class="text-gray-400 text-lg font-semibold mt-4">
-              AR Name System
-            </h4>
-            <p class="text-black text-xl mt-2 font-semibold tracking-wide">
-              $ArNS <span class="text-gray-400">0.00</span>
-            </p>
-          </div>
-
-          <div class="w-full mt-auto flex flex-col items-center justify-center">
-            <button
-              class="px-5 py-2 bg-gray-100 text-black rounded-full mt-4 font-semibold drop-shadow-sm
-               hover:bg-gray-200">Get ArNS tokens</button
-            >
-
-            <a href="/#/#" class="text-gray-400 mt-1">What is ArNS?</a>
-          </div>
-        </div>
-
-        <div class="flex flex-row items-center gap-2 mt-8">
-          <span class="text-lg font-semibold text-gray-400">...7dl6sK</span>
-          <button
-            ><img src="drop-down.png" alt="drop-down" width="25px" /></button
-          >
-        </div>
-      </div>
+      <DashboardBalances />
     </div>
   </div>
 
@@ -173,9 +87,8 @@
 
   <div
     class="flex flex-row items-center gap-2 w-[220px] mx-auto justify-center my-10 py-2 rounded-full
-  bg-gradient-to-r from-[#FF00E5] to-[#7B55EC] drop-shadow-lg hover:from-[#7B55EC] 
-  hover:to-[#FF00E5] cursor-pointer"
-  >
+    bg-gradient-to-r from-[#FF00E5] to-[#7B55EC] drop-shadow-lg hover:from-[#7B55EC] 
+    hover:to-[#FF00E5] cursor-pointer">
     <img src="magic-hand.png" alt="magic-hand" width="30px" />
     <span class="text-white font-semibold">Created on Compose</span>
   </div>
