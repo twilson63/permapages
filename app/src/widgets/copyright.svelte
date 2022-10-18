e129a877ffefa3cc1f267cf3d3b9498dae88ad98
mrsaifullah52 <mrsaifullah52@gmail.com>
2022-10-18 06:23:01 +0500

    copyright component added

---
+++ app/src/widgets/copyright.svelte
@@ -0,0 +1,7 @@
+<div
+  class="flex flex-row items-center gap-2 w-[220px] mx-auto justify-center my-10 mt-16 py-2 rounded-full
+bg-gradient-to-r from-[#FF00E5] to-[#7B55EC] drop-shadow-lg cursor-pointer"
+>
+  <img src="magic-hand.png" alt="magic-hand" width="30px" />
+  <span class="text-white font-semibold">Created on Compose</span>
+</div>
