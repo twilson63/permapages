dad28ca3092685a9350226aae67fb040498b4318
mrsaifullah52 <mrsaifullah52@gmail.com>
2022-10-30 07:02:55 +0500

    blogauthorpost: content updated

--- app/src/pages/author-preview.svelte
+++ app/src/pages/author-preview.svelte
@@ -9,79 +9,94 @@
   import WalletHelp from "../dialogs/help-wallet.svelte";
 
   let showless = true;
-  const postcomments = [
-    {
-      id: 1,
-      author: false,
-      name: "Godo17",
-      avtar: "./godo17.png",
-      walletid: "0x0000000000000000000000000000000000000000",
-      comment: `Thanks for sharing these notes. What is the latest with ANS 110
-              and the others in the pipeline? Curious to learn more for an EVM
-              compatible dApp I’m building with Arweave as the baselayer for the
-              whole dApp.`,
-      date: "2021-03-01T00:00:00.000Z",
-      version: 1,
-      ups: 9,
-      downs: 0,
-      replies: [
-        {
-          id: 2,
-          name: "Paula",
-          avtar: "./paula.png",
-          walletid: "0x0000000000000000000000000000000000000000",
-          comment: `I think those details are going to be shared very soon!`,
-          date: "2021-03-01T00:00:00.000Z",
-          version: 3,
-          ups: 2,
-          downs: 0,
-          replies: [],
-        },
-        {
-          id: 2,
-          name: "Paula",
-          avtar: "./paula.png",
-          walletid: "0x0000000000000000000000000000000000000000",
-          comment: `Hopefully tommorrow!`,
-          date: "2022-03-01T00:00:00.000Z",
-          version: 3,
-          ups: 1,
-          downs: 0,
-          replies: [],
-        },
-      ],
-    },
-    {
-      id: 3,
-      author: true,
+  const post = {
+    id: 1,
+    title: "Arweave Grow Crew #6",
+    date: "2021-03-01T00:00:00.000Z",
+    version: 3,
+    isAuthor: true,
+    author: {
       name: "Only Arweave",
+      username: "onlyarweave",
+      slogan: "Growing the Permaweb.",
       avtar: "./onlyarweave.png",
       walletid: "0x0000000000000000000000000000000000000000",
-      comment: `The release and all info related to this ANS 110 is slated 
-      to be released to the public in the second week of September. Coming 
-      soon, which is great as more devs use this!`,
-      date: "2022-08-01T00:00:00.000Z",
-      version: 3,
-      ups: 11,
-      downs: 0,
-      replies: [],
     },
-    {
-      id: 1,
-      author: false,
-      name: "Godo17",
-      avtar: "./godo17.png",
-      walletid: "0x0000000000000000000000000000000000000000",
-      comment: `Thanks for letting us.`,
-      date: "2022-09-01T00:00:00.000Z",
-      version: 3,
-      ups: 2,
-      downs: 0,
-      replies: [],
-    },
-  ];
-
-  let filteredcomments = postcomments.slice(0, 2);
+    content: ``,
+    tags: ["Arweave", "Grow Crew"],
+    comments: [
+      {
+        id: 1,
+        author: false,
+        name: "Godo17",
+        avtar: "./godo17.png",
+        walletid: "0x0000000000000000000000000000000000000000",
+        comment: `Thanks for sharing these notes. What is the latest with ANS 110
+                and the others in the pipeline? Curious to learn more for an EVM
+                compatible dApp I’m building with Arweave as the baselayer for the
+                whole dApp.`,
+        date: "2021-03-01T00:00:00.000Z",
+        version: 1,
+        ups: 9,
+        downs: 0,
+        replies: [
+          {
+            id: 2,
+            name: "Paula",
+            avtar: "./paula.png",
+            walletid: "0x0000000000000000000000000000000000000000",
+            comment: `I think those details are going to be shared very soon!`,
+            date: "2021-03-01T00:00:00.000Z",
+            version: 3,
+            ups: 2,
+            downs: 0,
+            replies: [],
+          },
+          {
+            id: 2,
+            name: "Paula",
+            avtar: "./paula.png",
+            walletid: "0x0000000000000000000000000000000000000000",
+            comment: `Hopefully tommorrow!`,
+            date: "2022-03-01T00:00:00.000Z",
+            version: 3,
+            ups: 1,
+            downs: 0,
+            replies: [],
+          },
+        ],
+      },
+      {
+        id: 3,
+        author: true,
+        name: "Only Arweave",
+        avtar: "./onlyarweave.png",
+        walletid: "0x0000000000000000000000000000000000000000",
+        comment: `The release and all info related to this ANS 110 is slated 
+        to be released to the public in the second week of September. Coming 
+        soon, which is great as more devs use this!`,
+        date: "2022-08-01T00:00:00.000Z",
+        version: 3,
+        ups: 11,
+        downs: 0,
+        replies: [],
+      },
+      {
+        id: 1,
+        author: false,
+        name: "Godo17",
+        avtar: "./godo17.png",
+        walletid: "0x0000000000000000000000000000000000000000",
+        comment: `Thanks for letting us.`,
+        date: "2022-09-01T00:00:00.000Z",
+        version: 3,
+        ups: 2,
+        downs: 0,
+        replies: [],
+      },
+    ],
+  };
+  let filteredcomments = post.comments.slice(0, 2);
 
   const showallcomments = () => {
     // display complete list of comments
@@ -97,7 +112,7 @@
     <PostStamp />
 
     <div class="w-full ml-4">
-      <PostPreviewHead />
+      <PostPreviewHead {post}/>
 
       <DemoPost />
 
@@ -137,7 +152,7 @@
       on:click={showallcomments}
     >
       {showless
-        ? `view all (${postcomments?.length}) replies`
+        ? `view all (${post.comments?.length}) replies`
         : "view less comments"}
     </button>
   </div>
