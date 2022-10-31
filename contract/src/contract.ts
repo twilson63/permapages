6fbfc9b2bdf3b898870b3664cdb1afa07812c690
Tom Wilson <tom@hyper.io>
2022-10-31 14:39:11 +0000

    add patch to fix transfer bug

--- contract/src/contract.ts
+++ contract/src/contract.ts
@@ -40,6 +40,9 @@ export async function handle(
     if (quantity <= 0 || caller === target) {
       throw new ContractError("Invalid token transfer.");
     }
+    if (!(caller in balances)) {
+      throw new ContractError("Caller doesn't own any balance.");
+    }
     if (balances[caller] < quantity) {
       throw new ContractError(
         "Caller balance not high enough to send " + quantity + " token(s)."
@@ -54,44 +57,6 @@ export async function handle(
     }
 
     return { state };
-  }
-
-  if (input.function === "readOutbox") {
-    // Ensure that a contract ID is passed
-    ContractAssert(!!input.contract, "Missing contract to invoke");
-
-    // Read the state of the foreign contract
-    const foreignState = await SmartWeave.contracts.readContractState(
-      input.contract
-    );
-
-    // Check if the foreign contract supports the foreign call protocol and compatible with the call
-    ContractAssert(
-      !!foreignState.foreignCalls,
-      "Contract is missing support for foreign calls"
-    );
-
-    // Get foreign calls for this contract that have not been executed
-    const calls: ForeignCallInterface[] = foreignState.foreignCalls.filter(
-      (element: ForeignCallInterface) =>
-        element.contract === SmartWeave.contract.id &&
-        !state.invocations.includes(element.txID)
-    );
-
-    // Run all invocations
-    let res: StateInterface = state;
-
-    for (const entry of calls) {
-      // Run invocation
-      res =
-        // @ts-expect-error
-        (await handle(res, { caller: input.contract, input: entry.input }))
-          .state;
-      // Push invocation to executed invocations
-      res.invocations.push(entry.txID);
-    }
-
-    return { state: res };
   }
 
   if (input.function === "balance") {
